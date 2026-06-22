7ec9bbb493ece24d623cf64cad579284cbba90a9
rodrigcasio <rod.casio@outlook.com>
2026-06-21 19:00:38 -0600

    `fix:` change port number and added `package-lock.json` file

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use('/courses', coursesRouter);
 app.get('/', (req, res) => res.send('CodeCraftHub API'));
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`CodeCraftHub API listening on port ${PORT}`);
 });
